bda85bb101bcb4b4221f64b50c8fa2e6e4be57a2
LuisP30 <pimenta.lh2@gmail.com>
2024-01-16 14:52:47 -0300

    valores primitivos e por referência

--- aula19/script.js
+++ aula19/script.js
@@ -6,3 +6,15 @@ São copiados quando passamos o valor deles para outra variável utilizando o si
 Referência - Array, Object e Functions (Valores mutáveis)
 São valores passados por referência (local na memória/"b" referencia o mesmo valor de "a" na memória)
 */
+
+let a ='A'
+let b = a
+a ='B'
+
+console.log(a, b)
+
+let c = [1, 2, 3]
+let d = c
+
+d.pop()
+console.log(c, d)
