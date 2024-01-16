dd88a93fd737436f2529ea63174944c96e6ff57f
LuisP30 <pimenta.lh2@gmail.com>
2024-01-16 00:00:44 -0300

    dados primitivos e por referência

---
+++ aula19/script.js
@@ -0,0 +1,8 @@
+// Valores primitivos e valores por referência
+/*
+Primitivos - string, number, boolean, undefined, null, bigint e symbol (Valores imutáveis)
+São copiados quando passamos o valor deles para outra variável utilizando o sinal de atribuição
+
+Referência - Array, Object e Functions (Valores mutáveis)
+São valores passados por referência (local na memória/"b" referencia o mesmo valor de "a" na memória)
+*/
