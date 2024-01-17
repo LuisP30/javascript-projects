13410a2abb031a5708476666ba49f59d6b1bf03f
LuisP30 <pimenta.lh2@gmail.com>
2024-01-17 16:17:18 -0300

    operadores de comparação

---
+++ logica_programacao/aula21/script.js
@@ -0,0 +1,13 @@
+// Operadores de comparação
+/*
+> Maior que
+< Menor que
+>= Maior que ou igual a
+<= Menor que ou igual a
+== igualdade (checa valor) (não recomendável o uso)
+=== igualdade estrita (checa valor e tipo)
+!= diferente (checa valor) (não recomendável o uso)
+!== diferente estrito (checa valor e tipo)
+*/
+console.log(5=='5') // True
+console.log(5==='5') // False
