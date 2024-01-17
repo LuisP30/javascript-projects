ee8a25ffefc90d766acb5560910dcb8886261ee4
LuisP30 <pimenta.lh2@gmail.com>
2024-01-17 16:49:53 -0300

    operadores lógicos

---
+++ logica_programacao/aula22/script.js
@@ -0,0 +1,11 @@
+/*
+Operadores lógicos
+
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+
+*/
+console.log(true && false) // AND Necessita que todas as condições sejam verdadeiras para ser verdadeiro
+console.log(true || false) // OR Necessita que apenas uma condição seja verdadeira para ser verdadeiro
+console.log(!true) // NOT inverte o resultado de uma expressão
