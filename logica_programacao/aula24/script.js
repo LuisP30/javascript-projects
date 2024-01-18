f7de165c41a57b610847145f11f5b23c23873f0f
LuisP30 <pimenta.lh2@gmail.com>
2024-01-18 12:23:03 -0300

    condicionais com if, else if e else

---
+++ logica_programacao/aula24/script.js
@@ -0,0 +1,15 @@
+// Condicionais
+const hora = 24;
+
+if(hora>=0 && hora<=11){
+    console.log('Bom dia')
+}
+else if(hora>=12 && hora<=17){
+    console.log('Boa tarde')
+}
+else if(hora>=18 && hora<=23){
+    console.log('Boa noite')
+}
+else{
+    console.log('Olá')
+}
