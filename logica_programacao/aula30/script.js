e27e34597a714b608d80e12d20247fa86b2f5d85
LuisP30 <pimenta.lh2@gmail.com>
2024-01-24 12:35:28 -0300

    estrutura de repetição (laço for)

---
+++ logica_programacao/aula30/script.js
@@ -0,0 +1,10 @@
+// Estrutura de repetição
+// Laço for
+for(let i=0; i<=10; i++){ // i+=10 para ir de 10 em 10
+    console.log(i)
+};
+// Percorrendo um array
+const frutas = ['Maçã', 'Pêra', 'Uva']
+for(let i=0; i<frutas.length; i++){
+    console.log(`Índice ${i}: ${frutas[i]}`)
+}
