bad977cf5e1b0ac46c9ebb63818e99f3d7e8efe5
LuisP30 <pimenta.lh2@gmail.com>
2024-01-24 23:56:56 -0300

    exercício de lógica utilizando arrow function

---
+++ logica_programacao/exercicio-02/script.js
@@ -0,0 +1,7 @@
+/* Escreva uma função chamada ePaisagem que
+recebe dois argumentos, largura e altura
+de uma imagem (number). Retorne true se a imagem
+estiver em modo paisagem */
+
+const ePaisagem = (larg, alt) => larg>alt
+console.log(ePaisagem(1080, 960))
