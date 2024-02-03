c613091bdf9c963d91c5fdc0cff21a5cd473ea70
LuisP30 <pimenta.lh2@gmail.com>
2024-02-02 21:10:23 -0300

    aprendendo forEach

---
+++ arrays/aula56/script.js
@@ -0,0 +1,5 @@
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// forEach - laço for apenas para arrays
+const a1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// Iterando com forEach
+a1.forEach((valor, indice, array) => console.log(valor, indice))
