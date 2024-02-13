3aa8ee2f5516050759d726d5a83fbb79265b116b
LuisP30 <pimenta.lh2@gmail.com>
2024-02-13 15:11:21 -0300

    modelo webpack

---
+++ tooling-ES6modules/modelo-webpack/index.js
@@ -0,0 +1,4 @@
+// Webpack
+// Ajuda a converter os scripts automaticamente
+// quando dou um npm i o node vai em meu arquivo package e baixa todos os modulos especificados
+// npm install --save-dev @babel/preset-env @babel/core @babel/cli babel-loader webpack webpack-cli regenerator-runtime core-js@2
