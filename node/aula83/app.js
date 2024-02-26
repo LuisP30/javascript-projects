6ffd1a7cdad836fed50803a5588c6dccfe8d380e
LuisP30 <pimenta.lh2@gmail.com>
2024-02-25 22:30:58 -0300

    Node Package Manager

---
+++ node/aula83/app.js
@@ -0,0 +1,31 @@
+/* https://docs.npmjs.com/about-npm
+Node Package Manager
+Iniciando o npm na pasta em que estou (Linha de comando):
+npm init -y (versão curta que é com -y)
+
+Instalando um pacote:
+npm install (nome do pacote)
+
+Buscando atualizações:
+npm update
+
+https://dev.to/stefanyrepetcki/diferenca-entre-devdependencies-dependencies-e-peerdependencies-5h0l
+Movendo um pacote para devDependecies no packge.json:
+npm install express --save-dev
+
+Movendo pacote para Dependecies:
+npm install express --save-prod
+
+Escolhendo a versão do pacote:
+npm install express@2.1.0
+
+formato de uma versão:
+2       1       0
+major   minor   patch
+
+Desinstalando um pacote:
+npm uninstall express
+
+Vendo pacotes desatualizados:
+npm outdated
+*/
