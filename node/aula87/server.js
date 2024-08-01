5dcf0a13e00b41ecdd1a4bde81a9068eb042dce5
LuisP30 <pimenta.lh2@gmail.com>
2024-08-01 19:50:06 -0300

    acessando body, params e query

--- node/aula87/server.js
+++ node/aula87/server.js
@@ -1,30 +1,38 @@
-// Importando o módulo express (para usar o nodemon: npx nodemon server.js)
 const express = require('express')
 
 // Inicializando o aplicativo Express
 const app = express()
 
-// Definindo uma rota para a página inicial do site
+// Para o express tratar o body das requisições
+app.use(
+    express.urlencoded(
+        {extended: true}
+    )
+)
+
 app.get('/', (request, response) => {
     response.send(`
         <form action="/" method="post">
-            Nome: <input type="text" /> 
+            Nome: <input type="text" name="nome" /> 
             <input type="submit" /> 
         </form>`)
-        // Envia a resposta "Hello" para o cliente quando acessa a rota raiz ('/')
 })
 
+// Acessando os dados recebidos de uma requisição post
 app.post('/', (request, response) => {
-    response.send(`Recebi seu formulário`)
+    console.log(request.body)
+    response.send(`Seu nome é ${request.body['nome']}`)
 })
 
-// Definindo uma rota para a página de contato
-app.get('/contato', (request, response) => {
-    response.send('Obrigado por entrar em contato') // Envia a resposta "Obrigado por entrar em contato" para o cliente quando acessa a rota '/contato'
+// Recebendo parâmetros na URL.
+// (Quando coloco o interrogação ao lado do parâmetro significa que ele é opcional)
+app.get('/testes/:id?', (req, res) => {
+    res.send(req.params)
+    console.log(req.query) // /testes/?nome=Luis&sobrenome=Henrique  (Acessando Query String)
 })
 
-// Configurando o servidor para ouvir na porta 3000
+
 const port = 3000
 app.listen(port, () => {
-    console.log(`Servidor ouvindo no endereço: http://127.0.0.1:${port}`) // Imprime uma mensagem informando que o servidor está ouvindo e em qual endereço
+    console.log(`Servidor ouvindo no endereço: http://127.0.0.1:${port}`) 
 })
