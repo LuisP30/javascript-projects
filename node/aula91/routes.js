f97ef002264eb6618511aa10ec0008b5c397377b
LuisP30 <pimenta.lh2@gmail.com>
2025-02-21 11:48:17 -0300

    aprendendo sobre middlewares

--- node/aula91/routes.js
+++ node/aula91/routes.js
@@ -1,9 +1,18 @@
 const express = require('express')
-
 const homeController = require('./src/controllers/homeController')
-
 const route = express.Router()
 
-route.get('/', homeController.paginaHomeGet)
+function meuMiddleware(req, res, next){
+    console.log('Middleware para antes da resposta')
+    req.session = { nome: 'Luis', sobrenome: 'Henrique' }
+    next() // next é utilizado para chamar o próximo middleware
+}
+
+route.get('/', meuMiddleware, homeController.paginaHomeGet, (req, res) => {
+    console.log('Middleware para depois da resposta')
+    console.log(req.session)
+})
+
+route.post('/', homeController.paginaHomePost)
 
 module.exports = route
--- node/aula91/src/controllers/homeController.js
+++ node/aula91/src/controllers/homeController.js
@@ -1,3 +1,10 @@
-exports.paginaHomeGet = (request, response) => {
+exports.paginaHomeGet = (request, response, next) => {
     response.render('index')
+    console.log('Middleware para resposta')
+    next()
+}
+
+exports.paginaHomePost = (request, response, next) => {
+    response.send(request.body)
+    next()
 }
