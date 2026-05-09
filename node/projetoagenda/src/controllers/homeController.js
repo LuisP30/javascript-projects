f697c05372590b0eeb96c12d4febb768585928d2
LuisP30 <pimenta.lh2@gmail.com>
2026-05-09 20:38:31 -0300

    remove log

--- node/projetoagenda/src/controllers/homeController.js
+++ node/projetoagenda/src/controllers/homeController.js
@@ -2,7 +2,6 @@ const contato = require('../models/ContatoModel')
 
 exports.index = async (req, res, next) => {
     const listaContatos = await contato.searchContatos()
-    console.log(listaContatos)
     res.render('index', { listaContatos })
     // console.log('Middleware que trata a requisição')
     next()
