1b67e441e9059949e940191279313e32f3d97024
LuisP30 <pimenta.lh2@gmail.com>
2026-05-23 15:11:35 -0300

    seed para usuários

---
+++ node/api_rest/src/database/seeds/20260523175237-criar-usuarios.js
@@ -0,0 +1,16 @@
+const bcryptjs = require('bcryptjs')
+
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.bulkInsert(
+    'users', [{
+    nome: 'Luis',
+    email: 'luis1@email.com',
+    password_hash: await bcryptjs.hash('123456', 8),
+    created_at: new Date(),
+    updated_at: new Date(),
+    }], {});
+  },
+
+  async down () {}
+};
